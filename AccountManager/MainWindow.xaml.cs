c1fd72fd3b198a634142c5bbc833f66b9feb0b4e
Eunyoung Kim <ekim88@my.centennialcollege.ca>
2020-01-26 00:40:57 -0500

    Created initial UI and transaction event

--- AccountManager/MainWindow.xaml.cs
+++ AccountManager/MainWindow.xaml.cs
@@ -24,5 +24,81 @@ namespace AccountManager
         {
             InitializeComponent();
         }
+
+        SavingAccount s = new SavingAccount();
+        ChequingAccount c = new ChequingAccount();
+
+        private void SavingRadio_Checked(object sender, RoutedEventArgs e)
+        { }
+
+        private void CheqRadio_Checked(object sender, RoutedEventArgs e)
+        { }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Saving Account
+            if (SavingRadio.IsChecked == true)
+            {
+                //Withdraw
+                if (WithdrawRadio.IsChecked == true)
+                {
+                    s.Withdraw(Convert.ToDouble(AmountTB.Text));
+                }
+                //Deposit
+                if (DepositRadio.IsChecked == true)
+                {
+                    s.Deposit(Convert.ToDouble(AmountTB.Text));
+                }
+
+                BalanceTB.Text = Convert.ToString(s.Balance);
+            }
+
+            //Chequing Account
+            if (CheqRadio.IsChecked == true)
+            {
+                //Withdraw
+                if (WithdrawRadio.IsChecked == true)
+                {
+                    c.Withdraw(Convert.ToDouble(AmountTB.Text));
+                }
+                //Deposit
+                if (DepositRadio.IsChecked == true)
+                {
+                    c.Deposit(Convert.ToDouble(AmountTB.Text));
+                }
+
+                BalanceTB.Text = Convert.ToString(c.Balance);
+            }
+        }
+
+        private void WithdrawRadio_Checked(object sender, RoutedEventArgs e)
+        { }
+
+        private void DepositRadio_Checked(object sender, RoutedEventArgs e)
+        { }
+    }
+}
+public class SavingAccount
+{
+    public double Balance { get; set; }
+    public void Withdraw(double amount)
+    {
+        Balance -= amount;
+    }
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+}
+public class ChequingAccount
+{
+    public double Balance { get; set; }
+    public void Withdraw(double amount)
+    {
+        Balance -= amount;
+    }
+    public void Deposit(double amount)
+    {
+        Balance += amount;
     }
 }
