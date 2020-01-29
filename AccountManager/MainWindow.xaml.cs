d7a7b0e1adde81f2688854df3a039450c9d49b46
Eunyoung Kim <ekim88@my.centennialcollege.ca>
2020-01-28 23:17:15 -0500

    Added input validation & transaction confirmation

--- AccountManager/MainWindow.xaml.cs
+++ AccountManager/MainWindow.xaml.cs
@@ -28,56 +28,104 @@ namespace AccountManager
         SavingAccount s = new SavingAccount();
         ChequingAccount c = new ChequingAccount();
 
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Validate empty input
+            if (AmountTB.Text == "")
+            {
+                MessageBox.Show("Please enter the amount for transaction", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (Convert.ToDouble(AmountTB.Text) <= 0)
+                {
+                    MessageBox.Show("Please check the amount and try again", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    //Saving Account
+                    if (SavingRadio.IsChecked == true)
+                    {
+                        //Withdraw
+                        if (WithdrawRadio.IsChecked == true)
+                        {
+                            if (Convert.ToDouble(AmountTB.Text) > s.Balance)
+                            {
+                                MessageBox.Show("You can not withdraw the amount which exceed your balance", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                            else
+                            {
+                                if (MessageBox.Show($"Do you want to withdraw ${AmountTB.Text} from your Saving account?", "Transaction Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                                {
+                                    s.Withdraw(Convert.ToDouble(AmountTB.Text));
+                                    BalanceTB.Text = Convert.ToString(s.Balance);
+                                }
+                            }
+                        }
+
+                        //Deposit
+                        if (DepositRadio.IsChecked == true)
+                        {
+                            if (MessageBox.Show($"Do you want to deposit ${AmountTB.Text} to your Saving account?", "Transaction Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                            {
+                                s.Deposit(Convert.ToDouble(AmountTB.Text));
+                                BalanceTB.Text = Convert.ToString(s.Balance);
+                            }
+                        }
+                    }
+
+                    //Chequing Account
+                    if (CheqRadio.IsChecked == true)
+                    {
+                        //Withdraw
+                        if (WithdrawRadio.IsChecked == true)
+                        {
+                            if (Convert.ToDouble(AmountTB.Text) > c.Balance)
+                            {
+                                MessageBox.Show("You can not withdraw the amount which exceed your balance", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                            else
+                            {
+                                if (MessageBox.Show($"Do you want to withdraw ${AmountTB.Text} from your Chequing account?", "Transaction Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                                {
+                                    c.Withdraw(Convert.ToDouble(AmountTB.Text));
+                                    BalanceTB.Text = Convert.ToString(c.Balance);
+                                }
+                            }
+                        }
+
+                        //Deposit
+                        if (DepositRadio.IsChecked == true)
+                        {
+                            if (MessageBox.Show($"Do you want to deposit ${AmountTB.Text} to your Chequing account?", "Transaction Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                            {
+                                c.Deposit(Convert.ToDouble(AmountTB.Text));
+                                BalanceTB.Text = Convert.ToString(c.Balance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
         private void SavingRadio_Checked(object sender, RoutedEventArgs e)
         { }
 
         private void CheqRadio_Checked(object sender, RoutedEventArgs e)
         { }
 
-        public void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //Saving Account
-            if (SavingRadio.IsChecked == true)
-            {
-                //Withdraw
-                if (WithdrawRadio.IsChecked == true)
-                {
-                    s.Withdraw(Convert.ToDouble(AmountTB.Text));
-                }
-                //Deposit
-                if (DepositRadio.IsChecked == true)
-                {
-                    s.Deposit(Convert.ToDouble(AmountTB.Text));
-                }
-
-                BalanceTB.Text = Convert.ToString(s.Balance);
-            }
-
-            //Chequing Account
-            if (CheqRadio.IsChecked == true)
-            {
-                //Withdraw
-                if (WithdrawRadio.IsChecked == true)
-                {
-                    c.Withdraw(Convert.ToDouble(AmountTB.Text));
-                }
-                //Deposit
-                if (DepositRadio.IsChecked == true)
-                {
-                    c.Deposit(Convert.ToDouble(AmountTB.Text));
-                }
-
-                BalanceTB.Text = Convert.ToString(c.Balance);
-            }
-        }
-
         private void WithdrawRadio_Checked(object sender, RoutedEventArgs e)
         { }
 
         private void DepositRadio_Checked(object sender, RoutedEventArgs e)
         { }
+
+        private void QuitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
     }
 }
+
 public class SavingAccount
 {
     public double Balance { get; set; }
